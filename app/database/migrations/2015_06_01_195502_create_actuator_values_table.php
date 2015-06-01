08f68dd0964adae396f64e4a2ddef6243d6b4905
Milos Simic <simkesd@gmail.com>
2015-06-01 22:59:10 +0200

    added migrations for sensors actuators and their values, and also their models

---
+++ app/database/migrations/2015_06_01_195502_create_actuator_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActuatorValuesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('actuator_values', function(Blueprint $table)
+		{
+            $table->increments('id');
+            $table->integer('actuator_id');
+            $table->string('value');
+            $table->string('value_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('actuator_values');
+	}
+
+}
---
+++ app/database/migrations/2015_06_01_195537_create_sensor_values_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSensorValuesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sensor_values', function(Blueprint $table)
+		{
+            $table->increments('id');
+            $table->integer('sensor_id');
+            $table->string('value');
+            $table->string('value_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('sensor_values');
+	}
+
+}
