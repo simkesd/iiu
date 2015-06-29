9728baeab8af84af2745f9b4ce065ec6ee0d204a
Milos Simic <simkesd@gmail.com>
2015-06-29 14:49:50 +0200

    improved seeders

--- app/database/seeds/ActuatorValueTableSeeder.php
+++ app/database/seeds/ActuatorValueTableSeeder.php
@@ -6,35 +6,78 @@ class ActuatorValueTableSeeder extends Seeder {
     {
         DB::table('actuator_values')->truncate();
 
+        $createdAt = new DateTime('now');
+        $createdAtDayBefore = new DateTime();
+        $createdAtDayBefore->modify('-1 day');
+        $createdAtTwoDaysBefore = new DateTime();
+        $createdAtTwoDaysBefore->modify('-2 days');
+
+        $updatedAt = $createdAt;
+        $updatedAtDayBefore = $createdAtDayBefore;
+        $updatedAtTwoDaysBefore = $createdAtTwoDaysBefore;
+
         ActuatorValue::create(array(
             'actuator_id' => '1',
             'value' => true,
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
         ));
 
         ActuatorValue::create(array(
             'actuator_id' => '1',
             'value' => false,
+            'created_at' => $createdAtDayBefore,
+            'updated_at' => $updatedAtDayBefore
+        ));
+
+        ActuatorValue::create(array(
+            'actuator_id' => '1',
+            'value' => true,
+            'created_at' => $createdAtTwoDaysBefore,
+            'updated_at' => $updatedAtTwoDaysBefore
         ));
 
         ActuatorValue::create(array(
             'actuator_id' => '3',
             'value' => false,
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
         ));
 
 
         ActuatorValue::create(array(
             'actuator_id' => '3',
+            'value' => true,
+            'created_at' => $createdAtDayBefore,
+            'updated_at' => $updatedAtDayBefore
+        ));
+
+        ActuatorValue::create(array(
+            'actuator_id' => '3',
             'value' => false,
+            'created_at' => $createdAtTwoDaysBefore,
+            'updated_at' => $updatedAtTwoDaysBefore
         ));
 
         ActuatorValue::create(array(
             'actuator_id' => '4',
             'value' => true,
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
         ));
 
         ActuatorValue::create(array(
             'actuator_id' => '4',
             'value' => false,
+            'created_at' => $createdAtDayBefore,
+            'updated_at' => $updatedAtDayBefore
+        ));
+
+        ActuatorValue::create(array(
+            'actuator_id' => '4',
+            'value' => true,
+            'created_at' => $createdAtTwoDaysBefore,
+            'updated_at' => $updatedAtTwoDaysBefore
         ));
     }
 
--- app/database/seeds/SensorValueTableSeeder.php
+++ app/database/seeds/SensorValueTableSeeder.php
@@ -7,7 +7,14 @@ class SensorValueTableSeeder extends Seeder {
         DB::table('sensor_values')->truncate();
 
         $createdAt = new DateTime('now');
+        $createdAtDayBefore = new DateTime();
+        $createdAtDayBefore->modify('-1 day');
+        $createdAtTwoDaysBefore = new DateTime();
+        $createdAtTwoDaysBefore->modify('-2 days');
+
         $updatedAt = $createdAt;
+        $updatedAtDayBefore = $createdAtDayBefore;
+        $updatedAtTwoDaysBefore = $createdAtTwoDaysBefore;
 
         SensorValue::create(array(
             'sensor_id' => '1',
@@ -19,12 +26,19 @@ class SensorValueTableSeeder extends Seeder {
         SensorValue::create(array(
             'sensor_id' => '1',
             'value' => 34,
-            'created_at' => $createdAt->modify('-1 day'),
-            'updated_at' => $updatedAt
+            'created_at' => $createdAtDayBefore,
+            'updated_at' => $updatedAtDayBefore
         ));
 
         SensorValue::create(array(
             'sensor_id' => '1',
+            'value' => 34,
+            'created_at' => $createdAtTwoDaysBefore,
+            'updated_at' => $updatedAtTwoDaysBefore
+        ));
+
+        SensorValue::create(array(
+            'sensor_id' => '2',
             'value' => 30,
             'created_at' => $createdAt,
             'updated_at' => $updatedAt
@@ -34,22 +48,15 @@ class SensorValueTableSeeder extends Seeder {
         SensorValue::create(array(
             'sensor_id' => '2',
             'value' => 30,
-            'created_at' => $createdAt->modify('-1 day'),
-            'updated_at' => $updatedAt
+            'created_at' => $createdAtDayBefore,
+            'updated_at' => $updatedAtDayBefore
         ));
 
         SensorValue::create(array(
             'sensor_id' => '2',
             'value' => 31,
-            'created_at' => $createdAt,
-            'updated_at' => $updatedAt
-        ));
-
-        SensorValue::create(array(
-            'sensor_id' => '2',
-            'value' => 28,
-            'created_at' => $createdAt->modify('-1 day'),
-            'updated_at' => $updatedAt
+            'created_at' => $createdAtTwoDaysBefore,
+            'updated_at' => $updatedAtTwoDaysBefore
         ));
     }
 
