1daa291bc35a922537c4b865848702f49beecfac
Milos Simic <simkesd@gmail.com>
2015-06-29 14:41:23 +0200

    more refactoring

--- app/database/seeds/SensorValueTableSeeder.php
+++ app/database/seeds/SensorValueTableSeeder.php
@@ -6,35 +6,50 @@ class SensorValueTableSeeder extends Seeder {
     {
         DB::table('sensor_values')->truncate();
 
+        $createdAt = new DateTime('now');
+        $updatedAt = $createdAt;
+
         SensorValue::create(array(
             'sensor_id' => '1',
             'value' => 27,
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
         ));
 
         SensorValue::create(array(
             'sensor_id' => '1',
             'value' => 34,
+            'created_at' => $createdAt->modify('-1 day'),
+            'updated_at' => $updatedAt
         ));
 
         SensorValue::create(array(
             'sensor_id' => '1',
             'value' => 30,
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
         ));
 
 
         SensorValue::create(array(
             'sensor_id' => '2',
             'value' => 30,
+            'created_at' => $createdAt->modify('-1 day'),
+            'updated_at' => $updatedAt
         ));
 
         SensorValue::create(array(
             'sensor_id' => '2',
             'value' => 31,
+            'created_at' => $createdAt,
+            'updated_at' => $updatedAt
         ));
 
         SensorValue::create(array(
             'sensor_id' => '2',
             'value' => 28,
+            'created_at' => $createdAt->modify('-1 day'),
+            'updated_at' => $updatedAt
         ));
     }
 
