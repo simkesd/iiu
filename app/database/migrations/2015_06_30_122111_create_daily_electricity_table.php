577f7c54572ae6b76a7909ea2d805bb2a7fa4cfa
Milos Simic <simkesd@gmail.com>
2015-06-30 14:25:14 +0200

    some migrations

---
+++ app/database/migrations/2015_06_30_122111_create_daily_electricity_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDailyElectricityTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+        Schema::create('daily_electricity', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->integer('actuator_id');
+            $table->string('kw_spent');
+            $table->string('cost');
+            $table->string('current_zone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('daily_electricity');
+	}
+
+}
